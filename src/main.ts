43b539d639b6d784aae37f52efab08e2efc5062e
sarrastambouli <sarahstambouli8@gmail.com>
2026-05-09 19:24:41 +0100

    hichemFront Angular 21 migration + material fix

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,17 @@
-//import { enableProdMode, importProvidersFrom } from '@angular/core';
-//import { environment } from './environments/environment';
-//import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-//import { AppRoutingModule } from './app/app-routing.module';
-//import { AppComponent } from './app/app.component';
-//import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'; // 👈 AJOUTER
-
-//if (environment.production) {
- // enableProdMode();
-//}
-
-//bootstrapApplication(AppComponent, {
-  //providers: [
-    //importProvidersFrom(BrowserModule, AppRoutingModule),
-    //provideHttpClient(withInterceptorsFromDi()) // 👈 AJOUTER
-  //]
-//}).catch((err) => console.error(err));
-import { enableProdMode } from '@angular/core';
-import { environment } from './app/ms6/environments/environment';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/ms6/app.module';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { environment } from './environments/environment';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'; // 👈 AJOUTER
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule, AppRoutingModule),
+    provideHttpClient(withInterceptorsFromDi()) // 👈 AJOUTER
+  ]
+}).catch((err) => console.error(err));
