f88285a884cae731ae2daad0fe257f954951054f
sarrastambouli <sarahstambouli8@gmail.com>
2026-05-12 22:44:47 +0100

    add CI/CD frontend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,14 +2,20 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   appVersion: packageInfo.version,
-  production: true,
+  production: false,
 
-  // API principale
-  apiUrl: 'http://localhost:8085/api',
+  gatewayUrl:  'http://localhost:8090',
+  apiUrl:      'http://localhost:8090/ms5/api', // ← défaut pour les services qui l'utilisent
 
-  // Configuration mock API
-  useMockDossierSanteApi: false,
+  hichemApi:   'http://localhost:8090/hichem/ms6/api',
+  projetsApi:  'http://localhost:8090/projets/api',
+  eventApi:    'http://localhost:8090/event/event_db/api',
+  backendPiApi:'http://localhost:8090/backend/api',
+  ms5Api:      'http://localhost:8090/ms5/api',
+  gesUserApi:  'http://localhost:8090/gesuser/api',
+  projetPiApi: 'http://localhost:8090/projetpi/api',
+  ecommerceApi:'http://localhost:8090/ecommerce/api',
 
-  // OpenRouter API
+  useMockDossierSanteApi: true,
   openRouterApiKey: 'YOUR_OPENROUTER_API_KEY_HERE'
 };
